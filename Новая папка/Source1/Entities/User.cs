4995b2606aa783717c5edfd17b5bbbcf636a9ea0
Илья <@DESKTOP-76I9O62>
2018-04-28 16:50:35 +0300

    del

--- Новая папка/Source1/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Source1.Entities
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-        public int DepartmentId { get; set; }
-    }
-}
--- Новая папка/Source2/Entities/Department.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Source2.Entities
-{
-    public class Department
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-    }
-}
